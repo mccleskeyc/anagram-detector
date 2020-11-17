b2bddeb3241ce42ffc5f060c6a59e9a5df54a2a2
mccleskeyc <mccleskeyc@gmail.com>
2020-11-17 14:28:18 -0500

    commit tree a4e18b1f0a912c4ae8dc811c5459ec8e9bdcb4e4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |word|
+            word.split("").sort == @word.split("").sort
+        end
+    end
+end
